b662342f448b5bba0cabc95bd887a9787bbb3c20
gertrudeaag <gertudeaag@gmail.com>
2026-08-27 16:08:39 +0000

    Fix time formatting in settings modal for short and long breaks

--- script.js
+++ script.js
@@ -149,8 +149,8 @@ const shortTime = document.getElementById("shortTime");
 const longTime = document.getElementById("longTime");
 
 settingsBtn.addEventListener("click", function() {
-    shortTime.value = `${String(Math.floor(DURATION.shortBreak / 60)).padStart(2, "0")}:${String(DURATION.shortBreak % 60).padStart(2, "0")}`;
-    longTime.value = `${String(Math.floor(DURATION.longBreak / 60)).padStart(2, "0")}:${String(DURATION.longBreak % 60).padStart(2, "0")}`;
+    shortTime.value = `${String(Math.floor(DURATION.shortBreak / 60)).padStart(2, "00")}:${String(DURATION.shortBreak % 60).padStart(2, "00")}`;
+    longTime.value = `${String(Math.floor(DURATION.longBreak / 60)).padStart(2, "00")}:${String(DURATION.longBreak % 60).padStart(2, "00")}`;
     settingsModal.showModal();
 });
 closeModalBtn.addEventListener("click", function() {
